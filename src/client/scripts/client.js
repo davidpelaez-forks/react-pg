b1194bd45563a4b17a9a8eeb84374d869f7a7326
David Pelaez <david@vlipco.co>
2015-10-12 17:28:22 -0500

    using react 13

--- src/client/scripts/client.js
+++ src/client/scripts/client.js
@@ -14,4 +14,4 @@ import { Provider } from 'react-redux';
 import store from '../../app/store';
 // Why does using react dom generate invarian ref error?
 // https://gist.github.com/jimfb/4faa6cbfb1ef476bd105
-React.render(<Provider store={store}><AppRoot/></Provider>,attachElement);
+React.render(<Provider store={store}>{()=><AppRoot/>}</Provider>,attachElement);
